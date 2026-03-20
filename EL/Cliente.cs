c5c3e12a7861fcbae1a6ee29a6ac401dd1d795ec
IngFranciscoMena <franciscodaniel.penate@unab.edu.sv>
2026-03-19 20:12:09 -0600

    se suben cambios de entidades, migraciones y organización del proyecto

--- EL/Cliente.cs
+++ EL/Cliente.cs
@@ -1,20 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EL
 {
+
+    // Data Anotations
     public class Cliente
     {
         // propiedades -> prop
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ClienteId { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string Nombres { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string Apellidos { get; set; }
+
+        [Required]
+        [StringLength(25)]
         public string Documento { get; set; }
+
+        [StringLength(120)]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [StringLength(30)]
         public string Telefono { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime2")]
         public DateTime FechaRegistro { get; set; }
     }
 }
